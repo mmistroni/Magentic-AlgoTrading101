d26eb296fe7dc607881e625784d9f6d390b8cb11
mmistroni <mmistroni@gmail.com>
2025-04-21 12:16:14 +0000

    small fix

--- adk_short_bot/agent.py
+++ adk_short_bot/agent.py
@@ -6,7 +6,7 @@ from .tools import count_characters
 root_agent = Agent(
     name="adk_short_bot",
     model="gemini-2.0-flash",
-    description="A bot that shortens message whiles maintaining their core meaning",
+    description="A bot that shortens message while  maintaining their core meaning",
     instructions=ROOT_AGENT_INSTRUCTIONS,
     tools=[count_characters],
 )
