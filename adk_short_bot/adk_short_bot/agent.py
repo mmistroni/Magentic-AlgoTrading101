804c9b5b4eaad1d85073d1cf7c8589e2c1d47ac4
mmistroni <mmistroni@gmail.com>
2025-04-21 12:50:33 +0000

    working example built my own way

--- adk_short_bot/adk_short_bot/agent.py
+++ adk_short_bot/adk_short_bot/agent.py
@@ -1,12 +1,11 @@
 from google.adk.agents import Agent
-from .prompts import ROOT_AGENT_INSTRUCTIONS
-from .tools import count_characters
-
+from adk_short_bot.prompts import ROOT_AGENT_INSTRUCTION
+from adk_short_bot.tools.character_counter import count_characters
 
 root_agent = Agent(
     name="adk_short_bot",
     model="gemini-2.0-flash",
-    description="A bot that shortens message while  maintaining their core meaning",
-    instructions=ROOT_AGENT_INSTRUCTIONS,
+    description="A bot that shortens messages while maintaining their core meaning",
+    instruction=ROOT_AGENT_INSTRUCTION,
     tools=[count_characters],
 )
