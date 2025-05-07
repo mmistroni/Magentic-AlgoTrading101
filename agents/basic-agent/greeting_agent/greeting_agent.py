d746bbc78ece6a718fa5e07cb76fd490a8179ea7
mmistroni <mmistroni@gmail.com>
2025-05-07 06:28:08 +0000

    renaming

--- agents/basic-agent/greeting_agent/greeting_agent.py
+++ agents/basic-agent/greeting_agent/greeting_agent.py
@@ -2,8 +2,8 @@ from google.adk.agents import Agent
 from .prompts import ROOT_AGENT_INSTRUCTION
 
 root_agent = Agent(
-    name="greeting-agent",
+    name="greeting_agent",
     model="gemini-2.0-flash",
-    description="A bot that shortens messages while maintaining their core meaning",
+    description="Greeting agent",
     instruction=ROOT_AGENT_INSTRUCTION
 )
