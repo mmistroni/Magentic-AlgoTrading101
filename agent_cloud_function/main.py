1cb538d8eb532bf6cb43cb9cd52c7d7eb405bbe4
mmistroni <mmistroni@gmail.com>
2025-06-13 06:27:53 +0000

    startin gclcoud function project

---
+++ agent_cloud_function/main.py
@@ -0,0 +1,18 @@
+def execute_call(request): 
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): The request object.
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return f'Hello {name}!
