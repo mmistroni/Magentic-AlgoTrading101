c2dc1db9e13a2a6f7475036aa7e0272105885c98
mmistroni <mmistroni@gmail.com>
2025-09-01 19:43:06 +0000

    sync

---
+++ agents/agent_team/agent_team/prompts.py
@@ -0,0 +1,6 @@
+ROOT_AGENT_INSTRUCTION = """
+You are a helpful assistant that can use the following tools:
+- get_current_time.
+The user will ask you for the current time and you must answer
+using the tools at your disposal.
+"""
