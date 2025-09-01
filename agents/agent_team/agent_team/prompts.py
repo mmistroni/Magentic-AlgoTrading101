186398d04bb9475943f9672d9787127ee3cd2173
mmistroni <mmistroni@gmail.com>
2025-09-01 19:46:24 +0000

    adding tools and prompts

--- agents/agent_team/agent_team/prompts.py
+++ agents/agent_team/agent_team/prompts.py
@@ -4,3 +4,13 @@ You are a helpful assistant that can use the following tools:
 The user will ask you for the current time and you must answer
 using the tools at your disposal.
 """
+# --- Define Model Constants for easier use ---
+
+# More supported models can be referenced here: https://ai.google.dev/gemini-api/docs/models#model-variations
+MODEL_GEMINI_2_0_FLASH = "gemini-2.0-flash"
+
+# More supported models can be referenced here: https://docs.litellm.ai/docs/providers/openai#openai-chat-completion-models
+MODEL_GPT_4O = "openai/gpt-4.1" # You can also try: gpt-4.1-mini, gpt-4o etc.
+
+# More supported models can be referenced here: https://docs.litellm.ai/docs/providers/anthropic
+MODEL_CLAUDE_SONNET = "anthropic/claude-sonnet-4-20250514" # You can also try: claude-opus-4-20250514 , claude-3-7-sonnet-20250219 etc
