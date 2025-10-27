6704640d8dc479ce34e491ba23069c607cd95f55
mmistroni <mmistroni@gmail.com>
2025-10-27 20:29:59 +0000

    updates

--- agents/agent_team/deploy/test_agent_setup.sh
+++ agents/agent_team/deploy/test_agent_setup.sh
@@ -1,14 +1,14 @@
 export APP_URL="https://agent-team-service-682143946483.us-central1.run.app"
 # Example: export APP_URL="https://adk-default-service-name-abc123xyz.a.run.app"
 export TOKEN=$(gcloud auth print-identity-token)
-
+# Need to sort out {"error": "Fail to load 'agent_team' module. No module named 'litellm'"}
 echo 'Listing Apps'
 
 curl -X GET -H "Authorization: Bearer $TOKEN" $APP_URL/list-apps
 
 echo 'Creating session......'
 curl -X POST -H "Authorization: Bearer $TOKEN" \
-    $APP_URL/apps/capital_agent/users/user_123/sessions/session_abc \
+    $APP_URL/apps/agent_team/users/user_123/sessions/session_abc \
     -H "Content-Type: application/json" \
     -d '{"state": {"preferred_language": "English", "visit_count": 5}}'
 
