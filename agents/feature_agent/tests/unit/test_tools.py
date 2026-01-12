d3abd8f3c4470499734a7313f2d4ac1f2e59197c
mmistroni <mmistroni@gmail.com>
2026-01-12 21:35:33 +0000

    sync

--- agents/feature_agent/tests/unit/test_tools.py
+++ agents/feature_agent/tests/unit/test_tools.py
@@ -1,6 +1,9 @@
 from feature_agent.tools import get_latest_prices_fmp
 from datetime import date
 
+# mock bq client to test the other method too
+
+
 def test_get_latest_price_fmp():
     start_date = date(2025, 12, 1)
     end_date = date(2025,12, 22)
