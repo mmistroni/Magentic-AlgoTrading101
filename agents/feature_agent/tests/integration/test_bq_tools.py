63476adb22e14981131f8060eb24908773698f8d
mmistroni <mmistroni@gmail.com>
2026-01-12 21:34:47 +0000

    adding bq test

--- agents/feature_agent/tests/integration/test_bq_tools.py
+++ agents/feature_agent/tests/integration/test_bq_tools.py
@@ -2,7 +2,7 @@ from feature_agent.tools import fetch_consensus_holdings_tool
 from datetime import date
 
 def test_fetch_consensus_holding_tool():
-    start_date = date(2025, 12, 1)
-    end_date = date(2025,12, 22)
-    res = get_latest_prices_fmp('AAPL', start_date, end_date)
-    print(f'---- Result is \n{res}')
+    filing_date = '2024-12-31'
+    res = fetch_consensus_holdings_tool(filing_date)
+    assert len(res) > 0
+    
